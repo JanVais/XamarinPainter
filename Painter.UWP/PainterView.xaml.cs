694c8823b21b8d4013bb977ee6ce49c1bc6e04a3
Miley Hollenberg <miley@nitrocrime.com>
2017-02-14 22:32:18 +0100

    Basic drawing working on UWP (that was easy)

---
+++ Painter.UWP/PainterView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Input.Inking;
+using Windows.UI.Input;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Painter.UWP
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class PainterView : UserControl
+	{
+		
+		public PainterView()
+		{
+			this.InitializeComponent();
+
+			InkDrawingAttributes inkDrawingAttributes = new InkDrawingAttributes();
+			inkDrawingAttributes.Color = Windows.UI.Colors.Blue;
+			inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(inkDrawingAttributes);
+
+			inkCanvas.InkPresenter.InputDeviceTypes = Windows.UI.Core.CoreInputDeviceTypes.Mouse | Windows.UI.Core.CoreInputDeviceTypes.Pen | Windows.UI.Core.CoreInputDeviceTypes.Touch;
+		}
+	}
+}
