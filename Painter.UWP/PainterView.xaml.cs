2a90908985c4e061043dff9ed055cd0583e27807
Miley Hollenberg <miley@nitrocrime.com>
2017-02-15 19:26:31 +0100

    UWP basics working (saving, loading and clearing)

--- Painter.UWP/PainterView.xaml.cs
+++ Painter.UWP/PainterView.xaml.cs
@@ -14,6 +14,13 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Input.Inking;
 using Windows.UI.Input;
+using Painter.Abstractions;
+using Windows.UI.Input.Inking.Core;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -24,7 +31,9 @@ namespace Painter.UWP
 	/// </summary>
 	public sealed partial class PainterView : UserControl
 	{
-		
+		List<Stroke> strokes;
+		Stroke currentStroke;
+
 		public PainterView()
 		{
 			this.InitializeComponent();
@@ -34,6 +43,87 @@ namespace Painter.UWP
 			inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(inkDrawingAttributes);
 
 			inkCanvas.InkPresenter.InputDeviceTypes = Windows.UI.Core.CoreInputDeviceTypes.Mouse | Windows.UI.Core.CoreInputDeviceTypes.Pen | Windows.UI.Core.CoreInputDeviceTypes.Touch;
+
+			CoreInkIndependentInputSource core = CoreInkIndependentInputSource.Create(inkCanvas.InkPresenter);
+			core.PointerPressing += Core_PointerPressing;
+			core.PointerMoving += Core_PointerMoving;
+			core.PointerReleasing += Core_PointerReleasing;
+
+			strokes = new List<Stroke>();
+			
+			DrawLines();
+		}
+
+		public string GetJson()
+		{
+			return JsonConvert.SerializeObject(strokes);
+		}
+
+		public async Task Clear()
+		{
+			await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+			{
+				inkCanvas.InkPresenter.StrokeContainer.Clear();
+			});
+		}
+
+		public void LoadJson(string json)
+		{
+			try
+			{
+				strokes = JsonConvert.DeserializeObject<List<Stroke>>(json);
+				DrawLines();
+			}
+			catch(Exception e)
+			{
+				Debug.WriteLine(e);
+			}
+		}
+
+		private async void DrawLines()
+		{
+			await Clear();
+			foreach(var stroke in strokes)
+			{
+				InkStrokeBuilder builder = new InkStrokeBuilder();
+				List<Windows.Foundation.Point> points = new List<Windows.Foundation.Point>();
+				stroke.Points.ForEach(x => points.Add(new Windows.Foundation.Point(x.X, x.Y)));
+				InkStroke curStroke = builder.CreateStroke(points);
+
+				InkDrawingAttributes strokeDrawingAttributes = new InkDrawingAttributes();
+				strokeDrawingAttributes.Color = Windows.UI.ColorHelper.FromArgb((byte)(stroke.StrokeColor.A * 255.0), (byte)(stroke.StrokeColor.R * 255.0), (byte)(stroke.StrokeColor.G * 255.0), (byte)(stroke.StrokeColor.B * 255.0));
+				curStroke.DrawingAttributes = strokeDrawingAttributes;
+
+				inkCanvas.InkPresenter.StrokeContainer.AddStroke(curStroke);
+			}
+		}
+
+		private void Core_PointerPressing(CoreInkIndependentInputSource sender, Windows.UI.Core.PointerEventArgs e)
+		{
+			var pos = e.CurrentPoint.Position;
+
+			currentStroke = new Stroke()
+			{
+				StrokeColor = new Color(0, 0, 1, 1),
+				Thickness = 1
+			};
+			currentStroke.Points.Add(new Abstractions.Point(pos.X, pos.Y));
+		}
+
+		private void Core_PointerMoving(CoreInkIndependentInputSource sender, Windows.UI.Core.PointerEventArgs e)
+		{
+			var pos = e.CurrentPoint.Position;
+			
+			currentStroke.Points.Add(new Abstractions.Point(pos.X, pos.Y));
+		}
+
+		private void Core_PointerReleasing(CoreInkIndependentInputSource sender, Windows.UI.Core.PointerEventArgs e)
+		{
+			var pos = e.CurrentPoint.Position;
+
+			currentStroke.Points.Add(new Abstractions.Point(pos.X, pos.Y));
+
+			strokes.Add(currentStroke);
 		}
 	}
 }
--- Testbed/PainterTestbed.UWP/MainPage.xaml.cs
+++ Testbed/PainterTestbed.UWP/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,28 @@ namespace PainterTestbed.UWP
         public MainPage()
         {
             this.InitializeComponent();
-        }
-    }
+
+			ApplicationView.GetForCurrentView().SetDesiredBoundsMode(ApplicationViewBoundsMode.UseCoreWindow);
+		}
+
+		private async void SaveJson(object sender, TappedRoutedEventArgs e)
+		{
+			Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+			Windows.Storage.StorageFile ticketsFile = await storageFolder.CreateFileAsync("image.json", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+			await Windows.Storage.FileIO.WriteTextAsync(ticketsFile, painterView.GetJson());
+		}
+
+		private async void Clear(object sender, TappedRoutedEventArgs e)
+		{
+			await painterView.Clear();
+		}
+
+		private async void LoadJson(object sender, TappedRoutedEventArgs e)
+		{
+			Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+			Windows.Storage.StorageFile ticketsFile = await storageFolder.CreateFileAsync("image.json", Windows.Storage.CreationCollisionOption.OpenIfExists);
+			string json = await Windows.Storage.FileIO.ReadTextAsync(ticketsFile);
+			painterView.LoadJson(json);
+		}
+	}
 }
